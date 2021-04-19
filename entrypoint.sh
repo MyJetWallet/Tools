2cf03e04e65bab52c439bef832dfbd170c9d787e
Alexey Novichikhin <32311883+NovichikhinAlexey@users.noreply.github.com>
2021-04-19 14:34:54 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ set -e
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 
-cat /tmp/config
 
 export KUBECONFIG=/tmp/config
 RN=$(kubectl describe deployment $POD -n  $NAMESPACE|grep NewReplicaSet:|awk '{print $2}')
